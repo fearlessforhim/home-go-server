49431f408df96f446824043be8238f1ec445c577
Jonathan Cameron <jonathancameron6@pm.me>
2023-01-20 18:19:44 +0000

    Intital commit with working server

---
+++ api/posts.go
@@ -0,0 +1,51 @@
+package api
+
+import (
+	"net/http"
+	"encoding/json"
+	"log"
+	"database/sql"
+	_ "modernc.org/sqlite"
+)
+
+type BlogPost struct {
+	Id int		`json:"id"`
+	Title string	`json:"title"`
+	Content string 	`json:"content"`
+	Timestamp int64 `json:"createdTime"`
+}
+
+func FetchPosts(w http.ResponseWriter, r *http.Request) {
+	var posts []BlogPost
+	
+	db, err := sql.Open("sqlite", "./blog.db")
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM BlogPost")
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var title string
+		var content string
+		var createdTime int64
+
+		err := rows.Scan(&id, &title, &content, &createdTime)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		post := BlogPost{Id: id, Title: title, Content: content, Timestamp: createdTime}
+	        posts = append(posts, post)
+
+	}
+
+	j, err := json.Marshal(posts)
+
+	if err != nil {
+                w.Write([]byte("An error occurred " + err.Error()))
+                return
+        }
+
+	w.Write([]byte(string(j)))
+}
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"net/http"
+	"github.com/go-chi/chi/v5"
+	"go-server/api"
+)
+
+func main() {
+
+	fmt.Println("Running main")
+
+	r := chi.NewRouter()
+
+	fmt.Println("Enabling routes")
+
+	r.Route("/api/blog", func(r chi.Router) {
+		r.Get("/getLatestPosts", api.FetchPosts)
+		r.Route("/posts", func(r chi.Router) {
+			r.Get("/", api.FetchPosts)
+		});
+	})
+
+	FileServer(r)
+
+	fmt.Println("Starting server on port 8080")
+
+	if err:=http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)	
+		fmt.Printf("An error occurred")
+	}
+}
+
+func FileServer(router *chi.Mux) {
+	root := "./static/fearlessforhim.github.io"
+	fs := http.FileServer(http.Dir(root))
+
+	router.Get("/*", func(w http.ResponseWriter, r *http.Request) {
+		if _, err := os.Stat(root + r.RequestURI); os.IsNotExist(err) {
+			http.StripPrefix(r.RequestURI, fs).ServeHTTP(w, r)
+		} else {
+			fs.ServeHTTP(w, r)
+		}
+	})
+}
